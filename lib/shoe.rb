01110592d87d9d34e60db7a60c36022c1ec75dbd
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-12 04:08:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 class Shoe 
-  
-  attr_reader :
+  attr_writer :color, :size, :material, :condition
+  attr_reader :brand
   def initialize(brand)
     @brand = brand
   end
