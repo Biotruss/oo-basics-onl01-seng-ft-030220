169c0a88979c1608ace752f73a92619bc5dafe5c
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-12 04:03:23 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+  attr_accessor :title
+  def initialize(title, author_name, page_count, genre)
+    @title = title
+    @author_name = author_name
+    @page_count = page_count
+    @genre = genre
+  end
+  def properties
+    @title = title
+    @author_name = author_name
+    @page_count = page_count
+    @genre = genre
+  end
+  
+  
 
+
+
+
+end
 
